7f4f16c69b866dcc533899a00ec906cb870011bd
AnushaGuruswamy <anushagxwokz@gmail.com>
2021-10-25 22:35:52 +0530

    default values

---
+++ basics/Mana.java
@@ -0,0 +1,7 @@
+class Mana{
+static byte HairColour;
+public static void main(String []args){
+
+System.out.println(HairColour);
+}
+}
---
+++ basics/Rock.java
@@ -0,0 +1,7 @@
+class Rock{
+static short size;
+public static void main(String []args){
+
+System.out.println(size);
+}
+}
