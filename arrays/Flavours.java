b932af839192f6d8cb1954591a85ce2280e29aee
AnushaGuruswamy <92967532+AnushaGuruswamy@users.noreply.github.com>
2021-10-26 17:08:28 +0530

    Add files via upload

---
+++ arrays/Flavours.java
@@ -0,0 +1,7 @@
+class Flavours{
+public static void main (String []args){
+char[] flavours = {'A','B','M','C'};
+char flavour=flavours[2];
+System.out.println(flavour);
+}
+}
---
+++ arrays/Flowers.java
@@ -0,0 +1,11 @@
+class Flowers{
+public static void main(String []args){
+
+byte []petals={4,5,7,9,10};
+byte petal=petals[2];
+System.out.println(petal);
+
+int size=petals.length;
+System.out.println(size);
+}
+}
