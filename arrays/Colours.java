d70d65aca862f90cb798945fac8eb9e228d47393
AnushaGuruswamy <anushagxwokz@gmail.com>
2021-10-26 17:02:34 +0530

    array files

---
+++ arrays/Colours.java
@@ -0,0 +1,10 @@
+class Colours{
+public static void main(String []args){
+char []colours={'B','Y','R','W','G'};
+char colour=colours[3];
+//System.out.println(colour);
+colours[3]='O';
+char newcolour=colours[3];
+System.out.println(newcolour);
+}
+}
---
+++ arrays/Cross.java
@@ -0,0 +1,7 @@
+class Cross{
+public static void main(String []args){
+boolean []go={true,false,false,false,true,false,true,true};
+boolean find=go[3];
+System.out.println(find);
+}
+}
