01ff123e04e74f2afda17494fd6e72b2fe7aed30
AnushaGuruswamy <92967532+AnushaGuruswamy@users.noreply.github.com>
2021-10-27 17:56:52 +0530

    if and else conditions

---
+++ conditional_statements/ExamHall.java
@@ -0,0 +1,21 @@
+class ExamHall{
+
+	
+	public static void main(String []args){
+	
+		byte bench = 5;
+	
+			System.out.println("Exam started");
+			System.out.println("15 minuts completed");
+			
+			if(bench>3){
+				System.out.println("start using chits");
+			}
+			else{
+				System.out.println("hide the chits");
+			}
+			
+			System.out.println("Exam completed");
+	}
+
+} 
---
+++ conditional_statements/Geezer.java
@@ -0,0 +1,22 @@
+class Geezer{
+
+	public static void main(String []args){
+	
+	float temperature = 40.05f;
+	
+		System.out.println("Turn on the Geezer");
+		System.out.println("Wait for 10 minutes");
+		System.out.println("Turn on the tap");
+		
+		if(temperature == 35.5f){
+		
+			System.out.println("Use the water");
+		}
+		else{
+		
+			System.out.println("Wait till the water reaches the temerature");
+		}
+		
+		System.out.println("Don't forget to turn off the Geezer after usage");
+	}
+}
