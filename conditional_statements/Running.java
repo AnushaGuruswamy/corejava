9a03f9319b86047a2be3ada59499d5ba71aa2731
AnushaGuruswamy <anushagxwokz@gmail.com>
2021-10-27 17:46:28 +0530

    if and else

---
+++ conditional_statements/Running.java
@@ -0,0 +1,33 @@
+class Running{
+
+	public static void main(String []args){
+	
+		double km = 1.5d;
+		double km1 = 5.0d;
+		
+		System.out.println("Good morning");
+		System.out.println("Start walking");
+		
+		if(km <= 2){
+		
+			System.out.println("Body is not yet ready to run");
+			System.out.println("Keep walking");
+		}
+		else{
+		
+			System.out.println("Start running");
+		}
+		
+		if(km1 == 5){
+		
+			System.out.println("Stop Running");
+			System.out.println("Morning health routine completed");
+		}
+		else{
+			
+			System.out.println("Morning routine need to be completed");
+			
+		}
+		
+	}
+}
