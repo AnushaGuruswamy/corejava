6d57975d0962ab3c603c512ddc29d0750d6f515f
AnushaGuruswamy <92967532+AnushaGuruswamy@users.noreply.github.com>
2021-10-31 18:05:41 +0530

    if else if statements and for loop

---
+++ if_else_if_and_for/Sweet.java
@@ -0,0 +1,29 @@
+class Sweet{
+	public static void main(String []args){
+	
+		String []type = {"Jamun","Rabdi","Badaam","Laddu","Kheer"};
+		char index = 'B';
+		
+		if(index == 'J'){
+			
+			System.out.println(type[0]);
+		}
+		
+		else if(index == 'R'){
+			
+			System.out.println("It's Rabdi");
+		}
+		
+		else if(index == 'B'){
+			
+			System.out.println(type[2]);
+		}
+		else if(index == 'L'){
+			
+			System.out.println("It's Motichor Laddu");
+		}
+		else if(index == 'K'){
+			System.out.println("It's Kheer");
+		}
+	} 
+}
