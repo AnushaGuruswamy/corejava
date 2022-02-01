b0949c2221a5e82091d723ab036cdcde1e4d5df8
AnushaGuruswamy <92967532+AnushaGuruswamy@users.noreply.github.com>
2022-02-01 15:51:37 +0530

    Delete basics directory

--- basics/Company.java
+++
@@ -1,7 +0,0 @@
-class Company{
-public static void main(String []args){
-byte c_id=56; 
-System.out.println(c_id);
-
-}
-}
--- basics/Mana.java
+++
@@ -1,7 +0,0 @@
-class Mana{
-static byte HairColour;
-public static void main(String []args){
-
-System.out.println(HairColour);
-}
-}
